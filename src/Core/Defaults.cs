d44346645564679ce51f7ed325c85acd7e3728be
Rafael Staib <me@rafaelstaib.com>
2019-01-02 12:47:57 +0100

    Fixed two sonar code smells

--- src/Core/Defaults.cs
+++ src/Core/Defaults.cs
@@ -4,10 +4,10 @@ namespace GreenDonut
 {
     internal static class Defaults
     {
-        public static readonly int CacheSize = 1000;
+        public const int CacheSize = 1000;
         public static readonly TimeSpan BatchRequestDelay = TimeSpan
             .FromMilliseconds(50);
-        public static readonly int MinimumCacheSize = 1;
+        public const int MinimumCacheSize = 1;
         public static readonly TimeSpan SlidingExpiration = TimeSpan.Zero;
     }
 }
